a8c4b1d9368cd57a37d7fadbd6a80b8879184746
Muhammad-Abrar2010 <tahseenbdserver@outlook.com>
2024-08-16 15:44:40 +0600

    added product list component

---
+++ src/Pages/Home/Components/ProductList.jsx
@@ -0,0 +1,14 @@
+
+import ProductCard from './ProductCard';
+
+const ProductList = ({ products }) => {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+            ))}
+        </div>
+    );
+};
+
+export default ProductList;
