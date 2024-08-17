d0a45d9066feaa1593fe8c2fbadd14a01f4c1d51
Muhammad-Abrar2010 <tahseenbdserver@outlook.com>
2024-08-17 18:32:29 +0600

    completed the project

--- src/Root/Components/Footer.jsx
+++ src/Root/Components/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer ()  {
     return (
         <footer className="bg-gray-300 py-5 text-center dark:bg-gray-500 dark:text-white">
-            <p>&copy; 2024 NavigateUI. All Rights Reserved.</p>
+            <p>&copy; 2024 Mega-Mart. All Rights Reserved.</p>
         </footer>
     );
 }
